f69aa0984c1c7f1421ee325e36588792af428461
praveen <praveensharma_se@outlook.com>
2024-05-02 15:14:35 +0530

    Add base component, add tasks components

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+
+// Local components
+import Task from "./components/Task";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>To-Do List Figma To React Native</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        {/* Today's Tasks */}
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        {/* Tasks Container */}
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          <Task text="Task 1" />
+          <Task text="Task 2" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#E8EAED",
+  },
+  tasksWrapper: {
+    padding: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
   },
 });
